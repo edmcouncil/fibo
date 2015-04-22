d0f30914ae793d120218a482584385348df01696
Jacobus Geluk <jacobus.geluk@gmail.com>
2015-04-22 17:35:10 -0400

    Update main.groovy

--- jenkins/job-dsl/main.groovy
+++ jenkins/job-dsl/main.groovy
@@ -1,15 +1,18 @@
 def owner = 'edmcouncil'
-def repoName = 'fibo'
-def repo = "${owner}/${repoName}"
-def forksApi = new URL("https://api.github.com/repos/${repo}/forks")
+def repo = 'fibo'
+//
+// Use the Github API to retrieve all forks of the FIBO repo
+// See https://developer.github.com/v3/repos/forks/
+//
+def forksApi = new URL("https://api.github.com/repos/${owner}/${repo}/forks")
 def forks = new groovy.json.JsonSlurper().parse(forksApi.newReader())
 
 forks.each {
-    def forkName = it.name
-    def jobName = "${repoName}-${forkName}".replaceAll('/','-')
+    def fork = it.name
+    def jobName = "${repo}-${fork}".replaceAll('/','-')
     job(jobName) {
         scm {
-            git("https://github.com/${forkName}.git", "master")
+            git("https://github.com/${fork}.git", "master")
         }        
     }
 }
