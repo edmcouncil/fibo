285e1f5cbdd4a76aea22cd075dc13a50bcb1ba6a
Jacobus Geluk <jacobus.geluk@gmail.com>
2015-05-24 15:14:57 -0400

    INFRA-63

---
+++ jenkins/stardog/load-fibo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Load the FIBO ontologies in the given clone of the Git repo (if run as a Jenkins job that's your current job workspace)
+# into Stardog.
+#
+
+fibo_repo_root=~/git/fibo
+
+
