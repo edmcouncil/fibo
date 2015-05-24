94320e74849f92fe232e345489409ea6766e7524
Jacobus Geluk <jacobus.geluk@gmail.com>
2015-05-24 15:46:22 -0400

    INFRA-63

--- jenkins/stardog/load-fibo.sh
+++ jenkins/stardog/load-fibo.sh
@@ -4,8 +4,29 @@
 # into Stardog.
 #
 
-fibo_repo_root=~/git/fibo
+if [ "${WORKSPACE}" == "" ] ; then
+  fibo_repo_root=~/git/fibo
+else
+	fibo_repo_root=${WORKSPACE}/fibo
+fi
+if [ ! -d "${fibo_repo_root}" ] ; then
+	echo "ERROR: Could not find FIBO repo root: ${fibo_repo_root}"
+	exit 1
+fi
 
-export
+if ! which stardog >/dev/null ; then
+  echo "ERROR: Stardog is not configured (or at least not on the PATH)"
+  exit 1
+fi
+
+stardog_bin=$(which stardog)
+
+fibo_test_named_graph=${BUILD_TAG}
+
+cd ${fibo_repo_root}
+
+set -x
+find . -type f -name '*.rdf' | xargs ${stardog_bin} data add fibo --named-graph ${fibo_test_named_graph}
+
 
 
