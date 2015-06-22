27683e04abac8208ee9436008f246ec3f95898de
Wei-Yeh Lee <weiyeh@InProTopia.com>
2015-06-22 18:46:11 -0400

    change temp.rdf path to use windows backslash instead

--- bin/analyze.bat
+++ bin/analyze.bat
@@ -1,5 +1,5 @@
 @echo off
-set PATH=%PATH%;c:\Users\weiyeh\Desktop\apps\pellet-2.3.1;d:\apps\apache-jena-2.10.1\bat;
+set PATH=%PATH%;c:\Documents\Software\Pellet\pellet-master;c:\documents\software\jena\apache-jena-2.13.0\bat;
 set JENA_HOME="d:\apps\apache-jena-2.10.1\"
 
-perl c:\users\weiyeh\desktop\projects\edmcouncil_fibo\argify.pl c:\users\weiyeh\desktop\projects\edmcouncil_fibo
+perl etc\argify.pl %*
