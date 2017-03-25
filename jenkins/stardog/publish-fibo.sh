37153de6cd4a65e19c7ed70428898b1385388e7c
Jacobus Geluk <jacobus.geluk@bnymellon.com>
2017-03-25 21:40:11 +0000

    INFRA-138 First stab at publish script
    INFRA-138 Better GIT_BRANCH handling
    
    
    INFRA-138 chmod set right now
    
    
    INFRA-138 Minor Change
    
    
    INFRA-138 More detailed directory structure
    
    
    INFRA-138 Replacing base IRIs
    
    
    INFRA-138 Disabling storing things in Stardog for now
    
    
    INFRA-138 Saving now
    
    
    INFRA-138 Better sed
    
    
    INFRA-138 Messing with tmp_dir
    
    
    INFRA-138 Better copy
    
    
    INFRA-138 See what goes wrong
    
    
    INFRA-138 Should work now
    
    
    INFRA-138 Preparing for rename to uppercase of all domains
    
    
    INFRA-138 Renamed domains to upper
    
    
    INFRA-138 Generate some more indexes (temporarily)
    
    
    INFRA-138 See what pandoc can do
    
    
    INFRA-138 Trying to fix
    
    
    INFRA-138 Trying again
    
    
    INFRA-138 Aargh
    
    
    INFRA-138 Make nix work and then pandoc
    
    
    INFRA-138 Also publishing pics now
    
    
    INFRA-138 Subtle bug

---
+++ jenkins/stardog/publish-fibo.sh
@@ -0,0 +1,181 @@
+#!/bin/bash
+#
+# Publish the FIBO ontologies
+#
+# This script needs to be run in a Jenkins workspace, where two git repos are cloned:
+#
+# - fibo (in ${WORKSPACE}/fibo directory)
+# - fibo-infra (in ${WORKSPACE}/fibo-infra directory)
+#
+#
+tmp_dir="/tmp"
+fibo_root=""
+fibo_infra_root=""
+
+spec_root="${WORKSPACE}/target"
+family_root="${spec_root}/fibo"
+product_root="${family_root}/ontology"
+branch_root=""
+
+stardog_vcs=""
+
+shopt -s globstar
+
+trap "rm -rf ${tmp_dir} >/dev/null 2>&1" EXIT
+
+#
+# This is for tools like pandoc
+#
+. /home/ec2-user/.nix-profile/etc/profile.d/nix.sh
+
+function initWorkspaceVars() {
+
+  tmp_dir=$(mktemp -d "${tmp_dir:-/tmp}/$(basename 0).XXXXXXXXXXXX")
+  echo tmp_dir=${tmp_dir}
+
+  fibo_root="${WORKSPACE}/fibo"
+  echo "fibo_root=${fibo_root}"
+
+  if [ ! -d "${fibo_root}" ] ; then
+    echo "ERROR: fibo_root directory not found (${fibo_root})"
+    return 1
+  fi
+
+  export fibo_infra_root="${WORKSPACE}/fibo-infra"
+  echo fibo_infra_root=${fibo_infra_root}
+
+  if [ ! -d "${fibo_infra_root}" ] ; then
+    echo "ERROR: fibo_infra_root directory not found (${fibo_infra_root})"
+    return 1
+  fi
+
+  rm -rf "${product_root}" >/dev/null 2>&1
+  mkdir -p "${product_root}"
+
+  return 0
+}
+
+function initGitVars() {
+
+  export GIT_COMMENT=$(cd ${fibo_root} ; git log --format=%B -n 1 ${GIT_COMMIT})
+  echo "GIT_COMMENT=${GIT_COMMENT}"
+
+  export GIT_AUTHOR=$(cd ${fibo_root} ; git show -s --pretty=%an)
+  echo "GIT_AUTHOR=${GIT_AUTHOR}"
+
+  export GIT_BRANCH=$(cd ${fibo_root} ; git rev-parse --abbrev-ref HEAD)
+  echo "GIT_BRANCH=${GIT_BRANCH}"
+
+  branch_root="${product_root}/${GIT_BRANCH}"
+
+  rm -rf "${branch_root}" >/dev/null 2>&1
+  mkdir "${branch_root}"
+
+  return 0
+}
+
+function initJiraVars() {
+
+  export JIRA_ISSUE=$(echo $GIT_COMMENT | rev | grep -oP '\d+-[A-Z0-9]+(?!-?[a-zA-Z]{1,10})' | rev)
+  echo JIRA_ISSUE=${JIRA_ISSUE}
+}
+
+function initStardogVars() {
+
+  export STARDOG_HOME=/usr/local/stardog-home
+  export STARDOG_BIN=/usr/local/stardog/bin
+
+  stardog_vcs="${STARDOG_BIN}/stardog vcs"
+}
+
+function copyRdfToTarget() {
+
+  echo "Copying all artifacts that we publish straight from git into target directory"
+
+  (
+    cd ${fibo_root}
+    cp **/*.{rdf,ttl,md,jpg,png} --parents ${branch_root}/
+  )
+
+  (
+    cd ${branch_root}
+    for domain in * ; do
+      [ -d ${domain} ] || continue
+      [ "${domain}" == "etc" ] && continue
+      [ "${domain}" == "ext" ] && continue
+      upperDomain=$(echo ${domain} | tr '[:lower:]' '[:upper:]')
+      [ "${domain}" == "${upperDomain}" ] && continue
+      echo Domain is ${domain} should be ${upperDomain}
+      mv ${domain} ${upperDomain}
+    done
+  )
+}
+
+function searchAndReplaceStuffInRdf() {
+
+  echo "Replacing stuff in RDF files"
+
+  local sedfile=$(mktemp ${tmp_dir}/sed.XXXXXX)
+  
+  cat > "${sedfile}" << __HERE__
+#
+# First replace all http:// urls to https:// if that's not already done
+#
+s@http://spec.edmcouncil.org@https://spec.edmcouncil.org@g
+#
+# Apparently there are some FND urls that are wrong in the git source:
+#  - https://spec.edmcouncil.org/FND/ should be
+#  - https://spec.edmcouncil.org/fibo/FND/
+#
+s@https://spec.edmcouncil.org/FND/@https://spec.edmcouncil.org/fibo/FND/@g
+s@\(https://spec.edmcouncil.org/fibo/\)@\1ontology/${GIT_BRANCH}/@g
+__HERE__
+
+  cat "${sedfile}"
+
+  (
+    set -x
+    find ${branch_root}/ -type f \( -name '*.rdf' -o -name '*.ttl' -o -name '*.md' \) -exec sed -i -f ${sedfile} {} \;
+  )
+
+  return 0
+}
+
+function convertMarkdownToHtml() {
+
+  echo "Convert Markdown to HTML"
+
+  (
+    set -x
+    cd "${branch_root}"
+    find . -type f -name '*.md' -exec pandoc --standalone --from markdown --to html -o {}.html {} \;
+  )
+
+  return 0
+}
+
+function storeVersionInStardog() {
+
+  echo "Commit to Stardog..."
+  ${stardog_vcs} commit --add $(find ${branch_root} -name "*.rdf") -m "$GIT_COMMENT" -u obkhan -p stardogadmin ${GIT_BRANCH}
+  SVERSION=$(${stardog_vcs} list --committer obkhan --limit 1 ${GIT_BRANCH} | sed -n -e 's/^.*Version:   //p')
+  ${stardog_vcs} tag --drop $JIRA_ISSUE ${GIT_BRANCH} || true
+  ${stardog_vcs} tag --create $JIRA_ISSUE --version $SVERSION ${GIT_BRANCH}
+}
+
+function main() {
+
+  initWorkspaceVars || return $?
+  initGitVars || return $?
+  initJiraVars || return $?
+  #initStardogVars || return $?
+
+  copyRdfToTarget || return $?
+  #storeVersionInStardog || return $?
+  searchAndReplaceStuffInRdf || return $?
+
+  convertMarkdownToHtml || return $?
+}
+
+main $@
+exit $?
