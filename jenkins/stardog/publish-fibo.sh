71db49a2d773bdbb91c5fb9cd3c5fdd4db89f115
Jacobus Geluk <jacobus.geluk@bnymellon.com>
2017-03-25 15:19:31 +0000

    INFRA-138 Better GIT_BRANCH handling

--- jenkins/stardog/publish-fibo.sh
+++ jenkins/stardog/publish-fibo.sh
@@ -8,22 +8,41 @@
 # - fibo-infra (in ${WORKSPACE}/fibo-infra directory)
 #
 #
+fibo_root=""
+fibo_infra_root=""
+stardog_vcs=""
 
-function initGitVars() {
+function initWorkspaceVars() {
 
-  export GIT_COMMENT=$(git log --format=%B -n 1 ${GIT_COMMIT})
-  echo GIT_COMMENT=${GIT_COMMENT}
+  fibo_root="${WORKSPACE}/fibo"
+  echo fibo_root=${fibo_root}
 
-  export GIT_AUTHOR=$(git show -s --pretty=%an)
-  echo GIT_AUTHOR=${GIT_AUTHOR}
-
-  if [ -z "${GIT_BRANCH}" ] ; then
-    echo "ERROR: GIT_BRANCH undefined"
+  if [ ! -d "${fibo_root}"] ; then
+    echo "ERROR: fibo_root directory not found (${fibo_root})"
     return 1
   fi
 
-  export GIT_BRANCH_SHORT="${GIT_BRANCH#*/}"
-  echo GIT_BRANCH_SHORT="${GIT_BRANCH_SHORT}"
+  export fibo_infra_root="${WORKSPACE}/fibo-infra"
+  echo fibo_infra_root=${fibo_infra_root}
+
+  if [ ! -d "${fibo_infra_root}"] ; then
+    echo "ERROR: fibo_infra_root directory not found (${fibo_infra_root})"
+    return 1
+  fi
+
+  return 0
+}
+
+function initGitVars() {
+
+  export GIT_COMMENT=$(cd ${fibo_root} ; git log --format=%B -n 1 ${GIT_COMMIT})
+  echo GIT_COMMENT=${GIT_COMMENT}
+
+  export GIT_AUTHOR=$(cd ${fibo_root} ; git show -s --pretty=%an)
+  echo GIT_AUTHOR=${GIT_AUTHOR}
+
+  export GIT_BRANCH=$(cd ${fibo_root} ; git rev-parse --abbrev-ref HEAD)
+  echo GIT_BRANCH=${GIT_BRANCH}
 }
 
 function initJiraVars() {
@@ -37,7 +56,7 @@ function initStardogVars() {
   # Need access to stardog or - send this to another agent
   export STARDOG_HOME=/mount/stardog/current
   
-  ""="${STARDOG_HOME}/bin/stardog vcs
+  stardog_vcs="${STARDOG_HOME}/bin/stardog vcs"
 }
 
 function generateBuildProperties() {
@@ -49,16 +68,17 @@ function generateBuildProperties() {
 
 function main() {
 
+  initWorkspaceVars || return $?
   initGitVars || return $?
   initJiraVars || return $?
   initStardogVars || return $
   generateBuildProperties || return $?
 
   echo "Commit to Stardog..."
-  ${stardog_vcs} commit --add $(find -name "*.rdf") -m "$GIT_COMMENT" -u obkhan -p stardogadmin ${GIT_BRANCH_SHORT}
-  SVERSION=$(${stardog_vcs} list --committer obkhan --limit 1 ${GIT_BRANCH_SHORT} | sed -n -e 's/^.*Version:   //p')
-  ${stardog_vcs} tag --drop $JIRA_ISSUE ${GIT_BRANCH_SHORT} || true
-  ${stardog_vcs} tag --create $JIRA_ISSUE --version $SVERSION ${GIT_BRANCH_SHORT}
+  ${stardog_vcs} commit --add $(find ${fibo_root} -name "*.rdf") -m "$GIT_COMMENT" -u obkhan -p stardogadmin ${GIT_BRANCH}
+  SVERSION=$(${stardog_vcs} list --committer obkhan --limit 1 ${GIT_BRANCH} | sed -n -e 's/^.*Version:   //p')
+  ${stardog_vcs} tag --drop $JIRA_ISSUE ${GIT_BRANCH} || true
+  ${stardog_vcs} tag --create $JIRA_ISSUE --version $SVERSION ${GIT_BRANCH}
   tree -H . > index.html
 }
 
