bf9ba8f2c4839e57ab76a8be468344e000168d33
Dean Allemang <dallemang@workingontologist.com>
2017-05-03 21:10:16 -0400

    INFRA-158 syntax

--- jenkins/bin/publish-fibo.sh
+++ jenkins/bin/publish-fibo.sh
@@ -562,7 +562,7 @@ function publishProductOntology() {
   ontologyCreateAboutFiles || return $?
   ontologySearchAndReplaceStuff || return $?
   ontologyConvertRdfToAllFormats || return $?
-  find ${tag_root}/ -type f -name "*.ttl" | while read file; TCBAnnotate "$file" ; done
+  find ${tag_root}/ -type f -name "*.ttl" | while read file; TCBAnnotate "$file"; done
   ontologyConvertMarkdownToHtml || return $?
 
   return 0
