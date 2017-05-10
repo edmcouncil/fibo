13f5df7d048187a2d755b3d45345cc9b482de595
Dean Allemang <dallemang@workingontologist.com>
2017-05-10 18:34:17 -0400

    INFRA-163 Try this for the ontology

--- jenkins/bin/publish-fibo.sh
+++ jenkins/bin/publish-fibo.sh
@@ -571,9 +571,6 @@ function zipWholeTagDir() {
     cd ${spec_root}
     set -x
     tar -cvzf "${tarGzFile}" "${tag_root/${spec_root}/.}"
-    zip -r ${zipttlFile} "${tag_root/${spec_root}}" -x \*.rdf \*.jsonld
-    zip -r ${ziprdfFile} "${tag_root/${spec_root}}" -x \*.ttl \*.jsonld
-    zip -r ${zipjsonldFile} "${tag_root/${spec_root}}" -x \*.rdf \*.ttl
   )
 
   echo "Created ${tarGzFile}:"
@@ -596,6 +593,19 @@ function copySiteFiles() {
   return 0
 }
 
+function zipOntologyFiles () {
+    local zipttlFile="${branch_root}/${GIT_TAG_NAME}.ttl.zip"
+    local ziprdfFile="${branch_root}/${GIT_TAG_NAME}.rdf.zip"
+    local zipjsonFile="${branch_root}/${GIT_TAG_NAME}.jsonld.zip"
+    
+  (
+    cd ${spec_root}
+    zip -r ${zipttlFile} "${tag_root/${spec_root}}" -x \*.rdf \*.jsonld
+    zip -r ${ziprdfFile} "${tag_root/${spec_root}}" -x \*.ttl \*.jsonld
+    zip -r ${zipjsonldFile} "${tag_root/${spec_root}}" -x \*.rdf \*.ttl
+    )
+}
+
 function publishProductOntology() {
 
   logRule "Publishing the ontology product"
@@ -608,6 +618,8 @@ function publishProductOntology() {
   ontologyConvertRdfToAllFormats || return $?
   ontologyAnnotateTopBraidBaseURL || return $?
   ontologyConvertMarkdownToHtml || return $?
+  zipOntologyFiles || return $?
+
 
   return 0
 }
