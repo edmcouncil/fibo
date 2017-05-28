21a6a74fceae150e3b1fe4ff940547aa61b24103
Jacobus Geluk <jacobus.geluk@bnymellon.com>
2017-05-28 20:14:45 +0100

    INFRA-170 First test with the new form

---
+++ site/fibo/emailform.js
@@ -0,0 +1,25 @@
+(function(){
+
+  emailjs.init("user_RAOldYPmmpjCfkcGi5jYL");
+
+  var myform = $("form#mail-form");
+
+  myform.submit(function(event){
+    event.preventDefault();
+
+    myform.find("button").text("Sending...");
+
+    var service_id = "amazon_ses";
+    var template_id = "spec_edmcouncil_org_feedback_form";
+
+    emailjs.sendForm(service_id,template_id,"myform")
+      .then(function(){
+        alert("Sent!");
+         myform.find("button").text("Send");
+      }, function(err) {
+         alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+         myform.find("button").text("Send");
+      });
+    return false;
+  });
+})();
