3e2c988f5c29c1b7be2872965044c61bd6397e63
Dean Allemang <dallemang@workingontologist.com>
2017-06-06 20:39:09 -0400

    INRA-175 Add in new About files

--- jenkins/bin/publish-fibo.sh
+++ jenkins/bin/publish-fibo.sh
@@ -222,13 +222,43 @@ function initStardogVars() {
 #
 function ontologyCreateAboutFiles () {
 
-  local tmpAboutFile="$(mktemp ${tmp_dir}/ABOUT.XXXXXX.ttl)"
+  local tmpAboutFileDev="$(mktemp ${tmp_dir}/ABOUTD.XXXXXX.ttl)"
+  local tmpAboutFileProd="$(mktemp ${tmp_dir}/ABOUTP.XXXXXX.ttl)"
   local echoq="$(mktemp ${tmp_dir}/echo.sparqlXXXXXX)"
 
   (
     cd ${tag_root}
 
-    cat > "${tmpAboutFile}" << __HERE__
+    cat > "${tmpAboutFileProd}" << __HERE__
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+@prefix owl: <http://www.w3.org/2002/07/owl#> 
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+<${tag_root_url}/AboutFIBO> a owl:Ontology;
+__HERE__
+
+
+    grep \
+	-r 'utl-av[:;.]Release' . | \
+	grep -F ".rdf" | \
+	sed 's/:.*$//'  | \
+	while read file; do
+	    grep "xml:base" "${file}";
+        done | \
+	sed 's/^.*xml:base="/owl:imports </;s/"[\t \n\r]*$/> ;/' \
+	    >> "${tmpAboutFileProd}"
+
+    cat > "${echoq}" << __HERE__
+CONSTRUCT {?s ?p ?o} WHERE {?s ?p ?o}
+__HERE__
+
+    "${jena_arq}" --data="${tmpAboutFileProd}" --query="${echoq}" --results=RDF > AboutFIBOProd.rdf
+  )
+
+  (
+    cd ${tag_root}
+
+    cat > "${tmpAboutFileDev}" << __HERE__
 @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
 @prefix owl: <http://www.w3.org/2002/07/owl#> 
@@ -244,14 +274,17 @@ __HERE__
       ) | \
       grep -vE "(catalog)|(About)" | \
 	  sed 's/^.*xml:base="/owl:imports </;s/"[ 	\n\r]*$/> ;/' \
-      >> "${tmpAboutFile}"
+      >> "${tmpAboutFileDev}"
 
     cat > "${echoq}" << __HERE__
 CONSTRUCT {?s ?p ?o} WHERE {?s ?p ?o}
 __HERE__
 
-    "${jena_arq}" --data="${tmpAboutFile}" --query="${echoq}" --results=RDF > AboutFIBO.rdf
+    "${jena_arq}" --data="${tmpAboutFileDev}" --query="${echoq}" --results=RDF > AboutFIBODev.rdf
   )
+
+
+
 }
 
 #
