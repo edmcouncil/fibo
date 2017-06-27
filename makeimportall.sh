e195e81b1b6ad4f6508ef6d4dfb2b852b2d0bf18
Dean Allemang <dallemang@workingontologist.com>
2017-06-27 16:58:09 -0400

    INFRA-162 Try to get the all generator to work better.

--- makeimportall.sh
+++ makeimportall.sh
@@ -1,9 +1,9 @@
 cat >all.ttl <<EOF
-# baseURI: https://spec.edmcounci.org/FIBO
-@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
-@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
-@prefix owl: <http://www.w3.org/2002/07/owl#> 
-@prefix xsd: <http://www.w3.org/2001/XMLSchema#> 
+# baseURI: https://spec.edmcounci.org/FIBO 
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>  .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>  .
+@prefix owl: <http://www.w3.org/2002/07/owl#>  .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> . 
 <https://spec.edmcounci.org/FIBO> a owl:Ontology . 
 EOF
 
