bf278cb160ca08bba4b9654b7d58b3f4d0e77666
Dean Allemang <dallemang@workingontologist.com>
2017-06-26 20:19:27 -0400

    INFRA-162 Move Guaranty to GuarantyExt

--- makeimportall.sh
+++ makeimportall.sh
@@ -1,11 +1,13 @@
 cat >all.ttl <<EOF
+# baseURI: https://spec.edmcounci.org/FIBO
 @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
 @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
 @prefix owl: <http://www.w3.org/2002/07/owl#> 
 @prefix xsd: <http://www.w3.org/2001/XMLSchema#> 
+<https://spec.edmcounci.org/FIBO> a owl:Ontology . 
 EOF
 
-grep -r "xml:base" bp der fnd ind ext md sec be cae civ fbc loan uo | grep -v catalog | grep -v About | sed 's/^.*xml:base="/owl:imports </;s/">/> ./' | sed 's@^@<https://spec.edmcounci.org/FIBO> @' >> all.ttl
+grep -r "xml:base" bp der fnd ind ext md sec be cae civ fbc loan uo | grep -v catalog | grep -v About | sed 's/^.*xml:base="/owl:imports </;s/">*.*$/> ./' | sed 's@^@<https://spec.edmcounci.org/FIBO> @' >> all.ttl
 
 
 
