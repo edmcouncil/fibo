1eb2b3a57b945272c73a5bb7a30d2c57476608af
Dean Allemang <dallemang@workingontologist.com>
2017-07-01 14:24:12 -0400

    Oops!  I left a debugging line in.

--- jenkins/bin/publish-fibo.sh
+++ jenkins/bin/publish-fibo.sh
@@ -487,9 +487,6 @@ EOF
 
   convertRdfFileTo rdf-xml "${outfile2}.rdf" "rdf-xml"
 
-  echo "here is the output from convertRdfFilTo"
-  cat "${outfile2}.rdf"
-  echo "that's the end of that file"
 
   mv -f "${outfile2}.rdf" "$1"
   rm "${outfile}.rdf"
