4ae61c5bcfc32592e30aac09ae1527bc85636642
Dean Allemang <dallemang@workingontologist.com>
2017-07-12 11:32:30 -0400

    INFRA-184 Typo on properties

--- jenkins/bin/publish-fibo.sh
+++ jenkins/bin/publish-fibo.sh
@@ -457,7 +457,7 @@ PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
 PREFIX afn: <http://jena.hpl.hp.com/ARQ/function#>
 CONSTRUCT {
-?cl rdfs:isDefinedBy ?clns . ?pr rdfs:isDefinedBy ?clns .
+?cl rdfs:isDefinedBy ?clns . ?pr rdfs:isDefinedBy ?prns .
 }
 WHERE {
 ?ont a owl:Ontology. 
