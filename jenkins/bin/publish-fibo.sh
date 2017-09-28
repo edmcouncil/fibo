9e7f2e26e6a6d0f43f6889b2a3138a8fac5fbf39
karthikeyan giriloganathan <karthikeyan.giriloganathan@bnymellon.com>
2017-09-28 11:29:20 +0530

    INFRA-191 temporarily stopped widoco to test vocabulary

--- jenkins/bin/publish-fibo.sh
+++ jenkins/bin/publish-fibo.sh
@@ -31,7 +31,7 @@ jena_arq=""
 #
 # ontology has to come before vocabulary because vocabulary depends on it.
 #
-products="ontology widoco vocabulary"
+products="ontology vocabulary"
 
 spec_root="${WORKSPACE}/target"
 family_root="${spec_root}/fibo"
