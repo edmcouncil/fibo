c4743406895cf4fdbc1d5f0bc02d870e604a4111
karthikeyan giriloganathan <karthikeyan.giriloganathan@bnymellon.com>
2017-09-28 11:38:43 +0530

    INFRA-191 Taking bak before removing first 3 lines

--- jenkins/bin/publish-fibo.sh
+++ jenkins/bin/publish-fibo.sh
@@ -1006,10 +1006,14 @@ function vocabyRunSpin() {
   #The first three lines contain some WARN statements - removing it to complete the build.
   #JC > Need to check why this happens
   echo "Removing the first three lines from temp1.ttl"
-  sed -i -e '1,3d' "${tmp_dir}/temp1.ttl"
+  sed -i.bak -e '1,3d' "${tmp_dir}/temp1.ttl"
 
   echo "Printing first 50 lines again"
-  head -n50 "${tmp_dir}/temp1.ttl"
+  head -n50 "${tmp_dir}/temp1.ttl.bak"
+
+  rm "${tmp_dir}/temp1.ttl"
+  mv "${tmp_dir}/temp1.ttl.bak" "${tmp_dir}/temp1.ttl"
+  ### END Karthik changes
 
   return 0
 }
