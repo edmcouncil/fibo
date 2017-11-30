1430f33e388374ff466a27e6a9df361159951171
Dean Allemang <dallemang@workingontologist.com>
2017-11-30 18:26:38 -0500

    INFRA-224 The first pass

---
+++ jenkins/bin/testgloss.sh
@@ -0,0 +1,22 @@
+arq $(find ../../fibo/be ../../fibo/bp ../../fibo/cae ../../fibo/civ ../../fibo/der ../../fibo/fbc ../../fibo/fnd ../../fibo/ind ../../fibo/loan ../../fibo/md ../../fibo/sec -name '*.rdf' | sed "s/^/--data=/") --data=../jenkins/bin/glossary/owlnames.ttl --data=../jenkins/bin/fibo-vocabulary/datatypes.rdf --query=../jenkins/bin/fibo-vocabulary/skosecho.sparql --results=TTL > temp0.ttl
+
+echo "finished combining"
+ 
+java -cp "../bin/apache-jena-2.11.0/lib/*;.;SPIN/spin-1.3.3.jar" org.topbraid.spin.tools.RunInferences http://example.org/example temp0.ttl > temp1.ttl
+
+echo "finished spin"
+
+  java \
+    -Xmx2G \
+    -Xms2G \
+    -jar "C:/Users/Dean/Documents/fibo/.git/hooks/rdf-toolkit.jar" \
+    --source temp1.ttl \
+    --source-format turtle \
+    --target temp2.json \
+    --target-format json-ld \
+    --infer-base-iri \
+    --use-dtd-subset \
+    > log 2>&1
+
+
+echo "finished json"
