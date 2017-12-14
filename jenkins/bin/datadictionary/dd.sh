ed98b5ba82bf5959391bcffb37b3655f423fd185
Dean Allemang <dallemang@acm.org>
2017-12-14 12:16:50 -0500

    INFRA-227 Speed up

--- jenkins/bin/datadictionary/dd.sh
+++ jenkins/bin/datadictionary/dd.sh
@@ -45,23 +45,9 @@ arq --data=combined.ttl --query=con1.sq --results=TSV > CONCEPTS
 #arq --data=combined.ttl --data=AllProd.ttl --query=pseudorange1.sq > pr1.ttl
 #sed -i "s/@en//g" pr1.ttl
 
-fi
 
-echo "finished pseudorange"
 
-echo "" > output.tsv
-
-cp CONCEPTS DONE
-
-function localdd () {
-
-if grep -q "$1" DONE ;
-then
-     echo "I've seen it before!"
-else
-echo "$1" >> DONE
-    
-cat > "temp.sq" << EOF
+cat > "ss.sq" << EOF
 
 PREFIX afn: <http://jena.apache.org/ARQ/function#>
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
@@ -72,11 +58,15 @@ prefix edm: <http://www.edmcouncil.org/temp#>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 PREFIX av: <https://spec.edmcouncil.org/fibo/FND/Utilities/AnnotationVocabulary/>
 
-SELECT DISTINCT ?table ?definition ?field ?description ?type
+
+#CONSTRUCT {[ a edm:Row; edm:table ?table; edm:definition ?definition; edm:field ?field; edm:description ?description; edm:type ?type; edm:class ?class; edm:next ?r1]}
+SELECT ?class ?table ?definition ?field ?description ?type ?r1
 WHERE {
-BIND ($1 AS ?class)
+?class a owl:Class .
+FILTER (ISIRI (?class))
+FIlTER (REGEX (xsd:string (?class), "edmcouncil"))
 ?class rdfs:subClassOf* ?base1 .
-?b1 a edm:PR ; edm:p ?p ; edm:pseudorange ?r1 ; edm:pseudodomain ?base1 .
+?b1 edm:pseudodomain ?base1; a edm:PR ; edm:p ?p ; edm:pseudorange ?r1  .
 ?p av:forDD "true"^^xsd:boolean .
 FILTER NOT EXISTS {?class rdfs:subClassOf* ?base2 .
 #                   FILTER (?base2 != ?base1)
@@ -86,61 +76,57 @@ FILTER NOT EXISTS {?class rdfs:subClassOf* ?base2 .
 
 ?p rdfs:label ?field .
 OPTIONAL {?p  skos:definition ?dx}
-LET (?description := COALESCE (?dx, "(none)"))
+BIND (COALESCE (?dx, "(none)") AS ?description )
 ?r1 rdfs:label ?type .
 ?class rdfs:label ?table
 OPTIONAL {?class skos:definition  ?dy }
-LET (?definition := COALESCE (?dy, "(none)"))
-
-} ORDER BY ?field
-
-
-EOF
-
-
-
-arq --data=combined.ttl --data=pr.ttl --data=AllProd.ttl --query=temp.sq --results=TSV > output1.tsv
-cat output1.tsv | tail -n +2 | sed "2,\$s/^[^\t]*\t[^\t]*\t/\t\"\"\t/" | sed "s/\"@../\"/g" >> output.tsv
-
-echo "Finished tsv for $1"
-
-cat > "temp.sq" << EOF
-
-PREFIX afn: <http://jena.apache.org/ARQ/function#>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
-prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
-prefix skos: <http://www.w3.org/2004/02/skos/core#> 
-prefix edm: <http://www.edmcouncil.org/temp#>
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-PREFIX av: <https://spec.edmcouncil.org/fibo/FND/Utilities/AnnotationVocabulary/>
-
-SELECT DISTINCT ?r1
-WHERE {
-BIND ($1 AS ?class)
-?class rdfs:subClassOf* ?base1 .
-?b1 a edm:PR ; edm:p ?p ; edm:pseudorange ?r1 ; edm:pseudodomain ?base1 .
-FILTER (ISIRI (?r1))
-?p av:forDD "true"^^xsd:boolean . 
-FILTER NOT EXISTS {?class rdfs:subClassOf* ?base2 .
-#                   FILTER (?base2 != ?base1)
-                   ?b2 a edm:PR ; edm:p ?p ; edm:pseudorange ?r2 ; edm:pseudodomain ?base2 .
-		   ?r2 rdfs:subClassOf+ ?r1 }
-
+BIND ( COALESCE (?dy, "(none)") AS ?definition )
 
 } 
 
 
 EOF
 
+arq --data=combined.ttl --data=pr.ttl --data=AllProd.ttl  --query=ss.sq --results=TSV | sed 's/"@../@/g' > ss.txt
 
-arq --data=combined.ttl --data=pr.ttl --data=AllProd.ttl --query=temp.sq --results=CSV > next
+### Store all this into a TDB so that we never haver to parse again
 
-echo "Finished next for $1"
+#rm -r TEMP
+#tdbloader2 --loc TEMP ss.ttl
+
+###
 
 
-tail -n +2  next |  while read uri ; do
-  localdd "<$(echo "${uri}" | sed 's/\r//')>" 
+fi
+
+echo "finished pseudorange"
+
+rm -r outputs
+mkdir outputs 
+echo "" > output.tsv
+
+cp CONCEPTS DONE
+
+
+
+function localdd () {
+
+if grep -q "$1" DONE ;
+then
+     echo "I've seen it before!"
+else
+echo "$1" >> DONE
+
+
+grep  "^$1" ss.txt | \
+sed "s/^[^\t]*\t//; s/\t[^\t]*$//; 2,\$s/^[^\t]*\t[^\t]*\t/\t\"\"\t/" >> output.tsv
+
+echo "Finished tsv sed for $1"
+
+
+#tdbquery --loc=TEMP  --query=temp2.sq --results=TSV   > next
+grep "^$1" ss.txt | sed 's/^.*\t//'  |  while read uri ; do
+  localdd "$(echo "${uri}" | sed 's/\r//')" 
 done
 
 
@@ -154,7 +140,7 @@ echo "$1"
 t=${1##*/}
 fname=${t%>*}
 echo $fname
-rm -f $fname.*
+rm -f outputs/$fname.*
 
 echo "" > output.tsv
 
@@ -163,22 +149,22 @@ cp CONCEPTS DONE
 localdd $1
 
 
-cat > $fname.csv <<EOF
+cat > outputs/$fname.csv <<EOF
 Table,Definition,Field,Field Definition,Type
 EOF
 
-sed 's/"\t"/","/g; s/^\t"/,"/' output.tsv > $fname.csv
+sed 's/"\t"/","/g; s/^\t"/,"/' output.tsv > $outputs/fname.csv
 
 
-cat > $fname.xls <<EOF
+cat > outputs/$fname.xls <<EOF
 <table border=1>
 <tr><th  bgcolor="goldenrod">Table</th><th  bgcolor="goldenrod">Definition</th><th  bgcolor="goldenrod">Field</th><th  bgcolor="goldenrod">Field Definition</th><th  bgcolor="goldenrod">Type</th></tr>
 EOF
-tail -n +2 output.tsv | sed 's!"\t"!</td><td valign="top">!g; s!^"!<td valign="top">!; s!^\t"!<td/><td valign="top">!; s!"$!</td>!g; s!^!<tr>!; s!$!</tr>!'  >> $fname.xls
-cat >>$fname.xls <<EOF
+tail -n +2 output.tsv | sed 's!"\t"!</td><td valign="top">!g; s!^"!<td valign="top">!; s!^\t"!<td/><td valign="top">!; s!"$!</td>!g; s!^!<tr>!; s!$!</tr>!'  >> outputs/$fname.xls
+cat >>$outputs/fname.xls <<EOF
 </table>
 EOF
-sed -i '4,${s/<td/<td bgcolor="azure"/g;n}' $fname.xls
+sed -i '4,${s/<td/<td bgcolor="azure"/g;n}' outputs/$fname.xls
 }
 
 
@@ -195,7 +181,7 @@ WHERE {
 
 EOF
 
-arq --data=combined.ttl --query=dumps.sq  --results=TSV> dumps
+arq  --data=combined.ttl --query=dumps.sq  --results=TSV> dumps
 
 
 tail -n +2 dumps | while read class ; do
