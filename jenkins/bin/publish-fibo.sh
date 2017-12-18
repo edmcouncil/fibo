da3a5fe8be571a3e49e78204316cba1bfd7e6869
Dean Allemang <dallemang@acm.org>
2017-12-18 14:28:14 -0500

    INFRA-227

--- jenkins/bin/publish-fibo.sh
+++ jenkins/bin/publish-fibo.sh
@@ -1956,7 +1956,7 @@ __HERE__
       sed "s@fibo/${fibo_rel}/\([a-zA-Z]*/\)@${family_product_branch_tag}/\U\1\E@" >> catalog-v001.xml
 
     cat  >> catalog-v001.xml << __HERE__
-a<!-- Automatically built by EDMC infrastructure -->
+<!-- Automatically built by EDMC infrastructure -->
 </catalog>
 __HERE__
 )    
@@ -1990,26 +1990,22 @@ function main() {
   if [ "$1" == "init" ] ; then
     return 0
   fi
-  echo "products = ${products}"
+
   for product in ${products} ; do
     case ${product} in
       ontology)
         publishProductOntology || return $?
         ;;
       widoco)
-	echo "not publishing widoco"    
-        # publishProductWidoco || return $?
+        publishProductWidoco || return $?
         ;;
       vocabulary)
-	echo "not publishing vocabulary"
-        # publishProductVocabulary || return $?
+        publishProductVocabulary || return $?
         ;;
       glossary)
-	echo "not publishing glossary"
-        # publishProductGlossary || return $?
+        publishProductGlossary || return $?
         ;;
       datadictionary)
-	echo "publishing data dictionary"    
         publishProductDataDictionary || return $?
         ;;
       *)
