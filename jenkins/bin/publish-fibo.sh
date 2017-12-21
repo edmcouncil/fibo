bf005a8b31811138019f32792b29a2463d8d0403
Dean Allemang <dallemang@acm.org>
2017-12-20 20:01:29 -0500

    INFRA-224 spelling

--- jenkins/bin/publish-fibo.sh
+++ jenkins/bin/publish-fibo.sh
@@ -1711,7 +1711,7 @@ GROUP BY ?c ?Term ?Type ?Definition ?GeneratedDefinition
 ORDER BY ?Term
 EOF
 
-arq --data="${glossary_root}/$1.ttl" --query=makecsv.sparql --results=TSV > "${glossary_root}/$2.tsv"
+arq --data="${glossary_root}/$1.ttl" --query="${tmp_dir}/makecsv.sparql" --results=TSV > "${glossary_root}/$2.tsv"
 
 sed -i 's/"@../"/g; s/\t\t/\t""\t/g; s/\t$/\t""/'  "${glossary_root}/$2.tsv"
 
