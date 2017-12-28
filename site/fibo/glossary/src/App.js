a8c395b6da7dd4ea5596e3eed085bc59e1bd43df
Jacobus Geluk <jacobus.geluk@gmail.com>
2017-12-28 18:04:41 +0000

    INFRA-225 Changed title of react app

--- site/fibo/glossary/src/App.js
+++ site/fibo/glossary/src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">FIBO Glossary</h1>
         </header>
         <SearchBox />
         <Table>
