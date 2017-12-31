889d7388df46147e4fd5a3418f448131673ff6b2
Jacobus Geluk <jacobus.geluk@gmail.com>
2017-12-31 14:14:05 +0000

    INFRA-225 Temporarily disabling offline use

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { registerServiceWorker, unregister } from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
-registerServiceWorker();
+//registerServiceWorker()
+unregister()
