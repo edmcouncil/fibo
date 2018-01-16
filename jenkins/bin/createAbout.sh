a0d9adb02e208adf8159fd045ecdd9fe647edd46
Dean Allemang <dallemang@workingontologist.com>
2018-01-16 14:36:05 -0500

    INFRA-253 Fix the env variables

--- jenkins/bin/createAbout.sh
+++ jenkins/bin/createAbout.sh
@@ -1,11 +1,14 @@
 source $(dirname ${0})/build-about.sh
-
-export tmp_dir="$(mktemp -d ./TMPXXXXXX)"
+export jena_arq=arq
 export tag_root=.
-export jena_arq=../fibo-infraX/bin/apache-jena-3.0.1/bin/arq
+export tmp_dir="."
+
 
 ontologyCreateAboutFiles 
-mv ${tmp_dir}/ABOUTP.*.ttl AboutFIBOProd.ttl
-mv ${tmp_dir}/ABOUTD.*.ttl AboutFIBODev.ttl
-rm -r "${tmp_dir}"
+
+
+rm ABOUTD.*.ttl
+rm ABOUTP.*.ttl
+rm echo.sparql*
+
 
