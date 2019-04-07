163fe948c5b74bcd0f7add96ecbee28fdd698534
Jacobus Geluk <jacobus.geluk@agnos.ai>
2019-04-07 16:31:12 +0100

    INFRA-414 Test with scm.userRemoteConfigs
    INFRA-414 Making sure that output & tmp directories are created
    
    
    INFRA-414 Switching off shallow cloning because of
    
    git error "refusing to merge unrelated histories"
    after force pushing a squashed commit into
    the PR-branch
    INFRA-414 Test with PR-status updater
    
    
    INFRA-414 Another test
    
    
    INFRA-414 Removed PR commit status update functions
    
    
    INFRA-414 No longer inherits fibo repo settings into LCC git checkout
    
    
    INFRA-414 Setting shallow clone depth to 50
    
    Trying to avoid the error "refusing to merge unrelated histories"
    INFRA-414 Testing without any shallow cloning

--- etc/process/vars/gitScript.groovy
+++ etc/process/vars/gitScript.groovy
@@ -1,54 +1,57 @@
 #!/usr/bin/env groovy
 
-def gitCheckOut(String repoName, String url, String credentialsId, branches) {
-
-  checkout([
-          $class                           : 'GitSCM',
-          branches                         : branches,
-          doGenerateSubmoduleConfigurations: false,
-          extensions                       : [
-                  [$class: 'LocalBranch', localBranch: '**'],
-                  [$class: 'CheckoutOption', timeout: 1],
-                  [$class: 'CloneOption', depth: 2, noTags: false, reference: "/var/lib/git/${repoName}", shallow: true, timeout: 1],
-                  [$class: 'AuthorInChangelog'],
-                  [$class: 'PruneStaleBranch'],
-                  [$class: 'IgnoreNotifyCommit']
-          ],
-          submoduleCfg                     : [],
-          userRemoteConfigs                : [
-                  [credentialsId: credentialsId, url: url]
-          ]
-  ])
-}
-
 def checkOutGitRepos() {
 
   final String gitCredentialsId = '50cac519-d41c-4765-8563-c43b7f55c877'
-  final String gitRepoUrlFIBO   = 'https://github.com/edmcouncil/fibo.git'
-  final String gitRepoUrlLCC    = 'https://github.com/edmcouncil/LCC.git'
 
   dir('input') {
     dir('fibo') {
-      gitCheckOut('fibo', gitRepoUrlFIBO, gitCredentialsId, scm.branches)
+      //
+      // The fibo repo is the main repo which also has configuration in the global variable scm,
+      // inherit those settings and add some more here.
+      //
+      checkout([
+        $class                           : 'GitSCM',
+        branches                         : scm.branches,
+        doGenerateSubmoduleConfigurations: false,
+        extensions                       : scm.extensions + [
+          [$class: 'LocalBranch', localBranch: '**'],
+          [$class: 'CheckoutOption', timeout: 1],
+//          [$class: 'CloneOption', depth: 50, noTags: false, reference: '/var/lib/git/fibo', shallow: false, timeout: 1],
+          [$class: 'AuthorInChangelog'],
+          [$class: 'PruneStaleBranch'],
+          [$class: 'IgnoreNotifyCommit']
+        ],
+        submoduleCfg                     : [],
+        userRemoteConfigs                : scm.userRemoteConfigs + [
+          [credentialsId: gitCredentialsId, url: 'https://github.com/edmcouncil/fibo.git']
+        ]
+      ])
     }
     dir('LCC') {
-      gitCheckOut('LCC', gitRepoUrlLCC, gitCredentialsId, [[name: '*/master']])
+      //
+      // The LCC repo is a secondary repo that we always use with its master branch. Do not inherit
+      // from the scm settings here.
+      //
+      checkout([
+        $class                           : 'GitSCM',
+        branches                         : [[name: '*/master']],
+        doGenerateSubmoduleConfigurations: false,
+        extensions                       : [
+          [$class: 'LocalBranch', localBranch: '**'],
+          [$class: 'CheckoutOption', timeout: 1],
+//          [$class: 'CloneOption', depth: 50, noTags: false, reference: '/var/lib/git/LCC', shallow: false, timeout: 1],
+          [$class: 'AuthorInChangelog'],
+          [$class: 'PruneStaleBranch'],
+          [$class: 'IgnoreNotifyCommit']
+        ],
+        submoduleCfg                     : [],
+        userRemoteConfigs                : [
+          [credentialsId: gitCredentialsId, url: 'https://github.com/edmcouncil/LCC.git']
+        ]
+      ])
     }
   }
 }
-
-def pullRequestStatus(String message) {
-
-  try {
-    echo "CHANGE_ID=${env.CHANGE_ID}"
-
-    if (env.BRANCH_NAME.startsWith('PR')) {
-      setGitHubPullRequestStatus state: currentBuild.currentResult, context: env.JOB_NAME, message: message
-    }
-  } catch(e) {
-    echo "ERROR: some error occurred in gitScript.pullRequestStatus: ${e}"
-  }
-}
-
 
 return this
