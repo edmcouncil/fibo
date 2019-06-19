b8f91fbb41f45a5f2570ef360868a41d90c556b4
Dean Allemang <dallemang@acm.org>
2019-06-19 15:06:03 -0400

    Dallemang/infra 417 (#732)
    * INFRA-417 Add current website to fibo repo
    
    * INFRA-417 Make a change to show that the site has been updated
    
    * INFRA-417  Remove all but fibopedia for testing
    
    * INFRA-417 Make Jenkinsfile use the latest docker
    
    * INFRA-417 Restore Jenkins file to include everything
    
    * INFRA-417  Set up a build without WIDOCO
    
    * INFRA-417 Stray characterin Jenkinsfile

---
+++ etc/site/fibo/emailform.js
@@ -0,0 +1,25 @@
+(function(){
+
+  emailjs.init("user_xPsSOXoaPy37HRI3vjCX6");
+
+  var myform = $("form#mail-form");
+
+  myform.submit(function(event){
+    event.preventDefault();
+
+    myform.find("button").text("Sending...");
+
+    var service_id = "amazon_ses";
+    var template_id = "spec_edmcouncil_org_feedback_form";
+
+    emailjs.sendForm(service_id,template_id,"mail-form")
+      .then(function(){
+        alert("Sent!");
+         myform.find("button").text("Send");
+      }, function(err) {
+         alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+         myform.find("button").text("Send");
+      });
+    return false;
+  });
+})();
