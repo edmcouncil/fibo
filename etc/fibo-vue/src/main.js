26c34810eb27750b5b46df3ca4c078caaed9e849
Karol Pasternak <karol.pasternak@makolab.com>
2019-10-16 22:13:36 +0200

    [INFRA-445] Add page tracking for GA

--- etc/fibo-vue/src/main.js
+++ etc/fibo-vue/src/main.js
@@ -4,8 +4,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: 'UA-124531442-2',
+  router
+})
 
 new Vue({
   router,
