146c471edd404eedffde67918be86e1be4bdfca7
Robert Trypuz <robert.trypuz@makolab.com>
2020-03-26 09:55:01 +0100

    Courses page has been updated. WIP

--- etc/fibo-vue/src/views/Courses.vue
+++ etc/fibo-vue/src/views/Courses.vue
@@ -2,292 +2,75 @@
   <div class="container">
     <main>
       <article>
-        <h1>
-          <span>Courses</span>
-        </h1>
-                <p class="text">Businesses today need to have comprehensive control and understanding of the data both within the enterprise and from outside. Regulatory pressure in many industries requires that an enterprise be able to find all relevant data about customers, products, business relations and in fact any aspect of the business quickly and accurately.  Advances in data analytics have shown that the more meaningful data an enterprise can bring to bear, the more accurately business drivers can be predicted. For this reason, many businesses are migrating from viewing their enterprise information as a collection of data to viewing it as an ongoing graph of meaningful knowledge. </p>
-
-
-                <p class="text">EDMC offers a <b>Knowledge Graph</b> couse consisting of the following four moduls:</p>
-                <ol>
-                    <li><a href="#KG-M1">Module 1: Knowledge Graph Fundamentals</a></li>
-                    <li><a href="#KG-M2">Module 2: Knowledge Graph Enterprise Framework</a></li>
-                    <li><a href="#KG-M3">Module 3: Knowledge Graph Modeling</a></li>
-                    <li><a href="#KG-M4">Module 4: Knowledge Graph Applied</a></li>
-                </ol>
-                
-
-                <h5><span>Course Objectives</span></h5>
-                <p>After finishing this course, you will know:</p>
-                <ol>
-                  <li>What a Knowledge Graph is,  what are its uses, and how does it provide business value.</li>
-                  <li>Different approaches to Knowledge Graphs, and when to use them</li>
-                  <li>Knowledge Graph standards and their importance in governance and application sustainability</li>
-                  <li>Categories of tools for building, deploying and maintaining knowledge graphs</li>
-                  <li>Best Practices for data publication, use and re-use</li>
-                  <li>How to build a simple example of a knowledge graph using sample data combined with public data.</li>
-                </ol>
-
-
-
-
-
-        <h3 id="KG-M1">
-          <span>Module 1: Knowledge Graph Fundamentals</span>
-        </h3>
-
-<p class="text">The technologies that support knowledge graphs go by many names: Semantic Web, Linked Data, Graph data, etc. Over the course of the past 20 years, the World Wide Web consortium has extended the specifications of the basic technology of the World Wide Web to include standards for managing knowledge graphs on a global scale.  These technologies are well-suited for the management of knowledge graphs in the setting of large, global companies whose data needs are distributed and diverse. </p>
-
-<h5><span>You will learn</span></h5>
-
+<div>
+<h1>
+<span>Invitation</span>
+</h1>
+<div style="text-align: justify;">
+<p>
+Businesses today need to have comprehensive control and understanding of the data both within the enterprise and from outside. Regulatory pressure in many industries requires that an enterprise be able to find all relevant data about customers, products, business relations and in fact any aspect of the business quickly and accurately. Advances in data analytics have shown that the more meaningful data an enterprise can bring to bear, the more accurately business drivers can be predicted. For this reason, many businesses are migrating from viewing their enterprise information as a collection of data to viewing it as an ongoing graph of meaningful knowledge.
+</p>
+</div>
+<h1>
+<span>Offer</span>
+</h1>
+<div style="text-align: justify;">
+<p>EDMC offers courses in Knowledge Graph, available in multiple forms:</p>
 <ol>
-  <li>The basics of Knowledge Graphs, who has been using them and for what purposes.</li>
-  <li>Business cases for Knowledge Graphs</li>
-  <li>Basic technology for Knowledge Graphs on a world-wide scale: RDF, RDFS, OWL, and SKOS</li>
-  <li>Query standards for Knowledge Graphs (SPARQL and SHACL)</li>
-  <li>The elements of a Knowledge Graph solution in an industrial setting</li>
-  <li>Specific examples of Knowledge Graph semantic standards: FIBO, AGROVOC, CDISC</li>
+<li>Instructor-led remote instruction by one of the EDM Council&rsquo;s certified trainers. These courses are open enrollment, registration at the EDM Council web site,</li>
+<li>Enterprise focused instruction. The same course material and instructor as our open enrollment course, but with the audience limited to knowledge workers from your organization. This allows more specific focus on use cases and industries of interest to your needs.</li>
+<li>Formal ELearning courses (to be available summer, 2020). The EDM Council is developing a Certification Package in Knowledge Graph. This is a deep-dive into the full details of knowledge graph technology and business practice.</li>
 </ol>
-
-<h5><span>This course is geared toward</span></h5>
-
-<ol>
-  <li>Data integration architects, designers, and developers</li>
-  <li>Data and technology architects</li>
-  <li>Chief Data Officers</li>
-</ol>
-
-
-<h5><span>Course Prerequisites</span></h5>
-
-<ol>
-  <li>Basics of enterprise data management</li>
-</ol>
-
-
-
-
-<h5><span>The curriculum covers the basics of the methodology and technology behind FIBO and will include </span></h5>
-
-
-<p><!-- #######  YAY, I AM THE SOURCE EDITOR! #########--></p>
-<ol>
-<li>Introduction</span><br />
-<ol>
-<li>What is a Knowledge Graph? </span></li>
-<li>History of Knowledge Graphs</span></li>
-<li>Knowledge Graphs on the Web</span></li>
-<li>Knowledge Graphs in the Enterprise</span><br />
+<p>In these challanging times EDMC will shortly issue&nbsp;<em>Open Knowledge graph and FIBO Fundamentals Course</em>.</p>
+<h5><span>Open Knowledge Graph and FIBO Fundamentals Course</span></h5>
+<p>This course will be virtually delivered on two consecutive days, 9:00am &ndash; 3:00pm each day&nbsp;with breaks.</p>
+<p>It will consists of 5 main modules:</p>
 <ul>
-<li>KG in data management, metadata management, data governance</span></li>
+<li>A. Knowledge Graph Business Case and Market Adoption [2:15]
+<ul>
+<li>A1. Knowledge Graph Perspective On Data</li>
+<li>A2. Knowledge Graph Industry Use Cases</li>
+<li>A3. Knowledge Graphs Takeaways</li>
 </ul>
 </li>
-</ol>
-</li>
-<li>Business Case for Knowledge Graph</span><br />
-<ol>
-<li>Use Cases for Knowledge Graph</span></li>
-<li>Knowledge graph examples from various industries</span><br />
+<li>B. Knowledge Graph Technical Framework [3:00]
 <ul>
-<li>Examples from FIBO (Financial Industry Business Ontology)</span></li>
-<li>AGROVOC</span></li>
-<li>CDISC</span></li>
+<li>B1. Knowledge Graph Enterprise Architecture Overview</li>
+<li>B2B3&B6. Knowledge Graph Data Languages (RDF, RDFS, SKOS)</li>
+<li>B4. Knowledge Graph Modeling Language (OWL)</li>
+<li>B5. Knowledge Graph Query language (SPARQL)</li>
+<li>B8. Knowledge Graph Enterprise Architecture Ecosystem</li>
 </ul>
 </li>
-<li>Knowledge Graph and Enterprise Data infrastructure (XML, Relational Data, etc.)</span></li>
-</ol>
+<li>C. FIBO Industry Model review [2:00]
+<ul>
+<li>C1. Structure</li>
+<li>C2. Content</li>
+<li>C3. Infrastructure</li>
+</ul>
 </li>
-<li>Semantic Web technology</span><br />
-<ol>
-<li>W3C Modeling Standards (RDF, RDFS, OWL)</span></li>
-<li>W3C Query standards (SPARQL, SHACL)</span></li>
-</ol>
+<li>D. Knowledge Graph Hands-on Exercises [2:00]
+<ul>
+<li>D1. Swaps in Finance</li>
+<li>D2. Toxic byproducts in Agriculture</li>
+<li>D3. Automotive manufacturing</li>
+</ul>
 </li>
-<li>Distributed Data and Data alignment</span><br />
-<ol>
-<li>Common reference</span></li>
-<li>Graph data</span></li>
-<li>Property Graphs, graph databases, and Knowledge Graphs</span></li>
-<li>Merging data</span></li>
-<li>Semantic Alignment</span></li>
-<li>&ldquo;Things vs. Strings&rdquo;</span></li>
-</ol>
+<li>E. Knowledge Graph Course Key Takeaways [0:15]
+<ul>
+<li>E1. Survey</li>
+<li>E2. Accreditation</li>
+</ul>
 </li>
-<li>Knowledge Graph Marketplace</span><br />
-<ol>
-<li>So you want to build a Knowledge Graph? </span></li>
-<li>Who provides What in a KG solution</span></li>
-</ol>
-</li>
-</ol>
-
-        <h3 id="KG-M2">
-          <span>Module 2: Knowledge Graph Enterprise Framework</span>
-        </h3>
-
-<p class="text">Enterprise Knowledge Graphs are enabled by a mature technology standardized by the W3C, the same organization that manages the standards that are the basis of the Web. The data standards are organized in layers, each building on the previous one.&nbsp; Together,these standards provide the infrastructure for an extensible knowledge graph, where datasets can be described in a way that allows them to interoperate in an enterprise setting.&nbsp;</p>
-
-<h5><span>You will learn</span></h5>
-<ul>
-<li>Details of the standards RDF, RDFS, OWL, SKOS, SPARQL and SHACL</li>
-<li>How to query semantic data sets</li>
-<li>Strategies for connecting data</li>
-<li>Patterns for using metadata resources (ontologies and vocabularies) to manage enterprise data</li>
-<li>Specifics of the FIBO standard</li>
 </ul>
-
-<h5><span>The course is geared toward</span></h5>
-<ul>
-<li>Data architects</li>
-<li>Data modelers</li>
-<li>Programmers who will build data-backed applications with a Knowledge Graph</li>
-</ul>
-
-<h5><span>Course prerequisites</span></h5>
-<ul>
-<li><a href="#KG-M1">Module 1: Knowledge Graph Fundamentals</a>, or</li>
-<li>Basic knowledge of Semantic Web standards, RDF, RDFS and SPARQL</li>
-</ul>
-
-<h5><span>Course outline</span></h5>
-<ol>
-<li>Technical details of RDF</li>
-<ol>
-<li>RDF serializations</li>
-<li>RDF parsers</li>
-<li>RDF databases (triple stores)</li>
-<li>Named graphs</li>
-<li>RDF and Property Graphs</li>
-</ol>
-<li>RDF Schema Language (RDFS)</li>
-<ol>
-<li>Domains and Ranges</li>
-<li>Subclasses and Subproperties</li>
-<li>Standard Annotations</li>
-<li>Data models in RDFS</li>
-</ol>
-<li>Web Ontology Language&nbsp;</li>
-<ol>
-<li>Inferencing in OWL</li>
-<li>Inverse and Symmetric properties</li>
-<li>Restrictions</li>
-</ol>
-<li>Simple Knowledge Organization System (SKOS)</li>
-<ol>
-<li>Concepts&nbsp;</li>
-<li>Semantic Relations</li>
-<li>Examples</li>
-<ol>
-<li>AGROVOC</li>
-<li>Library of Congress</li>
-</ol>
-</ol>
-<li>Ontology Design Patterns</li>
-<ol>
-<li>Ontologies and Vocabularies</li>
-<li>Reification&nbsp;</li>
-<li>Mediating entities and roles</li>
-</ol>
-<li>Example: FIBO</li>
-<ol>
-<li>Modular ontologies</li>
-<li>Roles and things that play them</li>
-<li>Identifiers</li>
-<li>Mapping tabular data to an ontology</li>
-</ol>
-<li>Querying a Knowledge Graph</li>
-<ol>
-<li>SPARQL</li>
-<li>Modeling with queries: SHACL</li>
-</ol>
-<li>Conclusion</li>
-<ol>
-<li>How to federate access across multiple data sources using graph queries</li>
-</ol>
-</ol>
-
-
-        <h3 id="KG-M3">
-          <span>Module 3: Knowledge Graph Modeling</span>
-        </h3>
-
-<h5><span>Course outline</span></h5>
-<ol>
-<li>Ontology Engineering</li>
-<li>Modeling Best Practices</li>
-<li>Ontology Reuse</li>
-<li>Evaluating Ontologies</li>
-</ol>
-</ol>
-
-
-        <h3 id="KG-M4">
-          <span>Module 4: Knowledge Graph Applied</span>
-        </h3>        
-
-<p class="text">Knowledge Graphs are based on mature technology.&nbsp; In this hands-on unit, students will create their own knowledge graph by merging three data sets:</p>
-<ol>
-<li>Permid (from Refinitiv)</li>
-<li>LEI data (from GLEIF)</li>
-<li>Sample Swaps data (from EDMC)</li>
-</ol>
-
-
-<p class="text">Students will convert the data sets to graphs (RDF),&nbsp; map them to FIBO, merge the graphs, then respond to business questions by formulating queries over the datasets.&nbsp;</p>
-
-<h5><span>You will learn</span></h5>
-<p></p>
-<ul>
-<li>How to convert data from other formats to graphs (RDF)</li>
-<li>How to merge multiple graphs in an RDF database (triple store)</li>
-<li>How to map data sets to an external ontology (FIBO)</li>
-<li>How to convert business questions into federated queries</li>
-</ul>
-
-<h5><span>The course is geared toward</span></h5>
-<ul>
-<li>Data architects</li>
-<li>Data application programmers</li>
-</ul>
-
-<h5><span>Prerequisites</span></h5>
-<ul>
-<li><a href="#KG-M2">Module 2: Knowledge Graph Enterprise Framework</a>, or</li>
-<li>Working knowledge of RDF, SPARQL and OWL</li>
-</ul>
-
-<h5><span>Course outline</span></h5>
-<ol>
-<li>Converting data to Graphs</li>
-<ol>
-<li>Tables (Spreadsheets)</li>
-<li>Tables (Relational Databases)</li>
-<li>XML</li>
-<li>JSON</li>
-</ol>
-<li>Mapping data to ontologies</li>
-<ol>
-<li>Reification of data</li>
-<li>Blank nodes and reified data</li>
-<li>Roles and Individuals, values and data</li>
-</ol>
-<li>Mapping examples:</li>
-<ol>
-<li>Interest rate swaps</li>
-<li>Legal Entity Identifiers</li>
-</ol>
-<li>Exercises: Queries over graph data</li>
-<ol>
-<li>Introduction to query platform</li>
-<li>Queries: Exploring the data</li>
-<li>Queries; Simplifying ownership data</li>
-<li>CONSTRUCT queries: Creating new graph data</li>
-<li>Queries: Who trades with Whom</li>
-<li>Queries: Ultimate parents</li>
-<li>Queries: Counterparty exposure</li>
-<li>&ldquo;Crosswalk&rdquo; data</li>
-</ol>
-</ol>
+<h1><span>Course Pallete</span></h1>
+<p><em>Open Knowledge graph and FIBO Fundamentals Course</em> is just part of our educational offer. Our full offer of courses covers different areas of Knowledge Graph Industry. These are shown below as a mind map that interlinks different learning topics.</p>
+<p> </p>
+<img style='height: 100%; width: 100%; object-fit: contain' src="../assets/img/course_palette.png" alt="FIBO Course Map" />
+<img style='height: 50%; width: 50%; object-fit: contain' src="../assets/img/course_palette_legend.png" alt="FIBO Course Map Legend" />
+<p>The map is split into six course modules, which are then split into smaller training blocks. Obviously, the modules and the blocks are not independent from one another as some require knowledge provided by others. Therefore the map shows which modules and blocks depend on which, i.e., it shows what you need to master before you can dig into something else.</p>
+</div>
+</div>
 
       </article>
     </main>
