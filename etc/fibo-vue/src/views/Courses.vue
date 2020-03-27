832c065e907c1e83b3452817bc3f8556c0bfaf5d
Robert Trypuz <robert.trypuz@makolab.com>
2020-03-27 10:41:14 +0100

    courses page update
    Signed-off-by: Robert Trypuz <robert.trypuz@makolab.com>

--- etc/fibo-vue/src/views/Courses.vue
+++ etc/fibo-vue/src/views/Courses.vue
@@ -1,96 +1,111 @@
 <template>
-  <div class="container">
-    <main>
-      <article>
-<div>
-<h1>
-<span>Invitation</span>
+    <div class="container">
+        <main>
+            <article>
+                <div>
+                    <h1>
+<span>Courses</span>
 </h1>
-<div style="text-align: justify;">
-<p>
-Businesses today need to have comprehensive control and understanding of the data both within the enterprise and from outside. Regulatory pressure in many industries requires that an enterprise be able to find all relevant data about customers, products, business relations and in fact any aspect of the business quickly and accurately. Advances in data analytics have shown that the more meaningful data an enterprise can bring to bear, the more accurately business drivers can be predicted. For this reason, many businesses are migrating from viewing their enterprise information as a collection of data to viewing it as an ongoing graph of meaningful knowledge.
-</p>
-</div>
-<h1>
-<span>Offer</span>
-</h1>
-<div style="text-align: justify;">
-<p>EDMC offers courses in Knowledge Graph, available in multiple forms:</p>
-<ol>
-<li>Instructor-led remote instruction by one of the EDM Council&rsquo;s certified trainers. These courses are open enrollment, registration at the EDM Council web site,</li>
-<li>Enterprise focused instruction. The same course material and instructor as our open enrollment course, but with the audience limited to knowledge workers from your organization. This allows more specific focus on use cases and industries of interest to your needs.</li>
-<li>Formal ELearning courses (to be available summer, 2020). The EDM Council is developing a Certification Package in Knowledge Graph. This is a deep-dive into the full details of knowledge graph technology and business practice.</li>
-</ol>
-<p>In these challanging times EDMC will shortly issue&nbsp;<em>Open Knowledge graph and FIBO Fundamentals Course</em>.</p>
-<h5><span>Open Knowledge Graph and FIBO Fundamentals Course</span></h5>
-<p>This course will be virtually delivered on two consecutive days, 9:00am &ndash; 3:00pm each day&nbsp;with breaks.</p>
-<p>It will consists of 5 main modules:</p>
-<ul>
-<li>A. Knowledge Graph Business Case and Market Adoption [2:15]
-<ul>
-<li>A1. Knowledge Graph Perspective On Data</li>
-<li>A2. Knowledge Graph Industry Use Cases</li>
-<li>A3. Knowledge Graphs Takeaways</li>
-</ul>
-</li>
-<li>B. Knowledge Graph Technical Framework [3:00]
-<ul>
-<li>B1. Knowledge Graph Enterprise Architecture Overview</li>
-<li>B2B3&B6. Knowledge Graph Data Languages (RDF, RDFS, SKOS)</li>
-<li>B4. Knowledge Graph Modeling Language (OWL)</li>
-<li>B5. Knowledge Graph Query language (SPARQL)</li>
-<li>B8. Knowledge Graph Enterprise Architecture Ecosystem</li>
-</ul>
-</li>
-<li>C. FIBO Industry Model review [2:00]
-<ul>
-<li>C1. Structure</li>
-<li>C2. Content</li>
-<li>C3. Infrastructure</li>
-</ul>
-</li>
-<li>D. Knowledge Graph Hands-on Exercises [2:00]
-<ul>
-<li>D1. Swaps in Finance</li>
-<li>D2. Toxic byproducts in Agriculture</li>
-<li>D3. Automotive manufacturing</li>
-</ul>
-</li>
-<li>E. Knowledge Graph Course Key Takeaways [0:15]
-<ul>
-<li>E1. Survey</li>
-<li>E2. Accreditation</li>
-</ul>
-</li>
-</ul>
-<h1><span>Course Pallete</span></h1>
-<p><em>Open Knowledge graph and FIBO Fundamentals Course</em> is just part of our educational offer. Our full offer of courses covers different areas of Knowledge Graph Industry. These are shown below as a mind map that interlinks different learning topics.</p>
-<p> </p>
-<img style='height: 100%; width: 100%; object-fit: contain' src="../assets/img/course_palette.png" alt="FIBO Course Map" />
-<img style='height: 50%; width: 50%; object-fit: contain' src="../assets/img/course_palette_legend.png" alt="FIBO Course Map Legend" />
-<p>The map is split into six course modules, which are then split into smaller training blocks. Obviously, the modules and the blocks are not independent from one another as some require knowledge provided by others. Therefore the map shows which modules and blocks depend on which, i.e., it shows what you need to master before you can dig into something else.</p>
-</div>
-</div>
 
-      </article>
-    </main>
-  </div>
+                    <h3>
+<span>Invitation</span>
+</h3>
+                    <div style="text-align: justify;">
+                        <p>
+                            Businesses today need to have comprehensive control and understanding of the data both within the enterprise and from outside. Regulatory pressure in many industries requires that an enterprise be able to find all relevant data about customers, products, business relations, and in fact, any aspect of the business quickly and accurately. Advances in data analytics have shown that the more meaningful data an enterprise can bring to bear, the more accurately business drivers can be predicted. For this reason, many businesses are migrating from viewing their enterprise information as a collection of data to viewing it as an ongoing graph of meaningful knowledge.
+                        </p>
+                    </div>
+                    <h3>
+<span>Offer</span>
+</h3>
+                    <div style="text-align: justify;">
+                        <p>EDMC offers courses in Knowledge Graph, available in multiple forms:</p>
+                        <ol>
+                            <li>Instructor led virtual training course by one of the EDM Council&rsquo;s certified trainers. These courses are open enrollment, registration at the EDM Council web site,</li>
+                            <li>Enterprise focused instruction. The same course material and instructor as our open enrollment course, but with the audience limited to knowledge workers from your organization. This allows a more specific focus on use cases and industries of interest to your needs.</li>
+                            <li>Formal ELearning courses (to be available summer, 2020). The EDM Council is developing a Certification Package in Knowledge Graph. This is a deep-dive into the full details of knowledge graph technology and business practice.</li>
+                        </ol>
+                    <h4>
+<span>Open Knowledge Graph and FIBO Fundamentals Course</span>
+</h4>                        
+                        <p>In these challenging times EDMC will provide on <b>April 28-20</b> and <b>May 20-21</b> with&nbsp;<b><em>Open Knowledge Graph and FIBO Fundamentals Course</em></b>.</p>
+                        <h5><span>Open Knowledge Graph and FIBO Fundamentals Course</span></h5>
+                        <p>This course will be virtually delivered on two consecutive days, 9:00am &ndash; 3:00pm Eastern Time each day&nbsp;with breaks.</p>
+                        <p>It will consist of 5 main modules:</p>
+                        <ul>
+                            <li><b>A. Knowledge Graph Business Case and Market Adoption</b> [2:15]
+                                <ul>
+                                    <li><b>A1.</b> Knowledge Graph Perspective On Data</li>
+                                    <li><b>A2.</b> Knowledge Graph Industry Use Cases</li>
+                                    <li><b>A3.</b> Knowledge Graphs Takeaways</li>
+                                </ul>
+                            </li>
+                            <li><b>B. Knowledge Graph Technical Framework</b> [3:00]
+                                <ul>
+                                    <li><b>B1.</b> Knowledge Graph Enterprise Architecture Overview</li>
+                                    <li><b>B2.</b> <b>B3.</b> & <b>B6.</b> Knowledge Graph Data Languages (RDF, RDFS, SKOS)</li>
+                                    <li><b>B4.</b> Knowledge Graph Modeling Language (OWL)</li>
+                                    <li><b>B5.</b> Knowledge Graph Query language (SPARQL)</li>
+                                    <li><b>B8.</b> Knowledge Graph Enterprise Architecture Ecosystem</li>
+                                </ul>
+                            </li>
+                            <li><b>C. FIBO Industry Model review</b> [2:00]
+                                <ul>
+                                    <li><b>C1.</b> Structure</li>
+                                    <li><b>C2.</b> Content</li>
+                                    <li><b>C3.</b> Infrastructure</li>
+                                </ul>
+                            </li>
+                            <li><b>D. Knowledge Graph Hands-on Exercises</b> [2:00]
+                                <ul>
+                                    <li><b>D1.</b> Swaps in Finance</li>
+                                    <li><b>D2.</b> Toxic byproducts in Agriculture</li>
+                                    <li><b>D3.</b> Automotive manufacturing</li>
+                                </ul>
+                            </li>
+                            <li><b>E. Knowledge Graph Course Key Takeaways</b> [0:15]
+                                <ul>
+                                    <li><b>E1.</b> Survey</li>
+                                    <li><b>E2.</b> Accreditation</li>
+                                </ul>
+                            </li>
+                        </ul>
+
+
+                        <h3><span>Course Palette</span></h3>
+                        <p><em>Open Knowledge graph and FIBO Fundamentals Course</em> is just part of our educational offer. Our full offer of courses covers different areas of Knowledge Graph Industry. These are shown below as a mind map that interlinks different learning topics.</p>
+                        <p> </p>
+                        <br>
+                        <img style='height: 100%; width: 100%; object-fit: contain' src="../assets/img/course_palette.png" alt="FIBO Course Map" />
+                        <img style='height: 50%; width: 50%; object-fit: contain' src="../assets/img/course_palette_legend.png" alt="FIBO Course Map Legend"/>
+                        <br>
+                        <p>The map is split into six course modules, which are then split into smaller training blocks. Obviously, the modules and the blocks are not independent from one another as some require knowledge provided by others. Therefore the map shows which modules and blocks depend on which, i.e., it shows what you need to master before you can dig into something else.</p>
+                    </div>
+                </div>
+
+            </article>
+        </main>
+    </div>
 </template>
 
 <script>
-import helpers from '../store/helpers.js';
-import { outboundClick, outboundLinkClick } from '../helpers/ga';
+    import helpers from '../store/helpers.js';
+    import {
+        outboundClick, outboundLinkClick
+    }
+    from '../helpers/ga';
 
-export default {
-  extends: helpers,
-  name: 'courses',
-  components: {},
-  methods: {
-    outboundClick,
-    outboundLinkClick,
-  },
-};
+    export default {
+        extends: helpers,
+        name: 'courses',
+        components: {},
+        methods: {
+            outboundClick,
+            outboundLinkClick,
+        },
+    };
 </script>
 
 <style lang="scss" scoped>
+
 </style>
